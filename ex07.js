41bdf68fe91d29f7ed8b4124ade8fd1c2db7241b
isabertolla <isatollana@gmail.com>
2026-08-03 12:11:46 -0300

    Delete ex07.js

--- ex07.js
+++
@@ -1,8 +0,0 @@
-// Sintaxe de Arrow Function ( (parâmetros) => retorno )
-const gerarRespostaHTTP = (status, mensagem) => {
-  return `HTTP Status ${status}: ${mensagem}`;
-};
-
-// Executando a função
-const resultado = gerarRespostaHTTP(200, "Sucesso ao buscar produtos");
-console.log(resultado);
