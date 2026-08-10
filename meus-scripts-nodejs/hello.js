ea4c4f8818f388a56b902f09853700ef86fe4fa5
isabertolla <isatollana@gmail.com>
2026-08-10 12:00:05 -0300

    Add files via upload

---
+++ meus-scripts-nodejs/hello.js
@@ -0,0 +1,4 @@
+console.log("Olá, mundo! Este é o meu primeiro script em Node.js!");
+console.log("Parabens! Você está programando!");
+console.log("Node.js versão:", process.version);
+console.log("Sistema operacional:", process.platform);
---
+++ meus-scripts-nodejs/minha-padaria-app/app.js
@@ -0,0 +1,6 @@
+const colors = require('colors');
+
+
+console.log('🍞 Bem-vindo á Padaria da Isabelly!'.rainbow);
+console.log('Pães frescos todos os dias!.'.green);
+console.log('Horário: 6h às 18h'.blue);
